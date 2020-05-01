660850001aa2c342a1b42bee3fdcaed1e6c27d83
Attol8 <jacopoattolini@gmail.com>
2020-05-01 16:23:52 +0200

    first commit

---
+++ settings.py
@@ -0,0 +1,64 @@
+import os
+import pandas as pd
+
+COMPETITION = 'm5-forecasting-accuracy'
+
+DIR_DATA = 'input_data'
+NOTEBOOK_DIR = 'notebooks'
+OUTPUT_DIR = 'output'
+
+#output data 
+FEATURE_DIR = os.path.join(OUTPUT_DIR, 'feature')
+METRIC_DIR = os.path.join(OUTPUT_DIR, 'metric')
+MODEL_DIR = os.path.join(OUTPUT_DIR, 'model')
+
+# output data - directories for the cross validation and ensembling
+VAL_DIR = os.path.join(OUTPUT_DIR, 'val')
+TST_DIR = os.path.join(OUTPUT_DIR, 'tst')
+SUBMISSION_DIR = os.path.join(OUTPUT_DIR, 'submission')
+
+#input data
+MAIN_DATA = os.path.join(DIR_DATA, 'sales_train_validation.csv')
+TRAIN_DATA = os.path.join(DIR_DATA, 'train.feather')
+TEST_DATA = os.path.join(DIR_DATA, 'test.feather')
+SAMPLE_SUBMISSION = os.path.join(DIR_DATA, 'sample_submission.csv')
+PRICES_DATA = os.path.join(DIR_DATA, 'sell_prices.csv')
+CALENDAR_DATA = os.path.join(DIR_DATA, 'calendar.csv')
+
+#constants
+TARGET = 'sales'            # Our target
+
+# #LIMITS and const
+
+# START_TRAIN = 1069               # We can skip some rows (Nans/faster training)
+# END_TRAIN   = 1913               # End day of our train set
+# P_HORIZON   = 28                 # Prediction horizon
+# USE_AUX     = True               # Use or not pretrained models
+
+# #FEATURES to remove
+# ## These features lead to overfit
+# ## or values not present in test set
+# remove_features = ['id','state_id','store_id',
+#                    'date','wm_yr_wk','d',TARGET]
+# mean_features   = ['enc_cat_id_mean','enc_cat_id_std',
+#                    'enc_dept_id_mean','enc_dept_id_std',
+#                    'enc_item_id_mean','enc_item_id_std'] 
+
+
+# # AUX(pretrained) Models paths
+# AUX_MODELS = '../input/m5-aux-models/'
+
+
+# #STORES ids
+# STORES_IDS = pd.read_csv(ORIGINAL+'sales_train_validation.csv')['store_id']
+# STORES_IDS = list(STORES_IDS.unique())
+
+
+# #SPLITS for lags creation
+# SHIFT_DAY  = 28
+# N_LAGS     = 15
+# LAGS_SPLIT = [col for col in range(SHIFT_DAY,SHIFT_DAY+N_LAGS)]
+# ROLS_SPLIT = []
+# for i in [1,7,14]:
+#     for j in [7,14,30,60]:
+#         ROLS_SPLIT.append([i,j])
---
+++ utils.py
@@ -0,0 +1,92 @@
+import os
+import settings
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+def make_out_directories(output_path = settings.OUTPUT_DIR):
+    if os.path.exists(output_path):
+        print(output_path + ' : exists')
+    else:
+        print('Making Directories')
+        os.mkdir(output_path)
+        dir_l = ['feature', 'metric', 'model', 'val', 'tst', 'submission']
+        for dir in dir_l:
+            os.mkdir(os.path.join(output_path, dir))
+
+def make_test_train():
+    #categories' types
+    CAL_DTYPES={"event_name_1": "category", "event_name_2": "category", "event_type_1": "category", 
+    "event_type_2": "category", "weekday": "category", 'wm_yr_wk': 'int16', "wday": "int16",
+    "month": "int16", "year": "int16", "snap_CA": "float32", 'snap_TX': 'float32', 'snap_WI': 'float32' }
+    PRICE_DTYPES = {"store_id": "category", "item_id": "category", "wm_yr_wk": "int16","sell_price":"float32" }
+
+    prices = pd.read_csv(settings.PRICES_DATA, dtype=PRICE_DTYPES)
+    cal = pd.read_csv(settings.CALENDAR_DATA, dtype=CAL_DTYPES)
+    last_day = 1913
+    nrows=None
+    first_day = 1069 # If you want to load all the data set it to '1' -->  Great  memory overflow  risk ! 1069 is start of 2014 (minus 2.5 years)
+
+    #change data types
+    for col, col_dtype in PRICE_DTYPES.items():
+        if col_dtype == "category":
+            prices[col] = prices[col].cat.codes.astype("int16")
+            prices[col] -= prices[col].min()
+            
+    cal["date"] = pd.to_datetime(cal["date"])
+    for col, col_dtype in CAL_DTYPES.items():
+        if col_dtype == "category":
+            cal[col] = cal[col].cat.codes.astype("int16")
+            cal[col] -= cal[col].min()
+
+    numcols = [f"d_{day}" for day in range(first_day,last_day+1)]
+    catcols = ['id', 'item_id', 'dept_id','store_id', 'cat_id', 'state_id']
+    dtype = {numcol:"float32" for numcol in numcols} 
+    dtype.update({col: "category" for col in catcols if col != "id"})
+
+    #melt the dataframe (tidier data)
+    dt = pd.read_csv(settings.MAIN_DATA, 
+                        nrows = nrows, usecols = catcols + numcols, dtype = dtype) #load the train dataset
+
+    for col in catcols:
+        if col != "id":
+            dt[col] = dt[col].cat.codes.astype("int16")
+            dt[col] -= dt[col].min()
+
+    #include test data
+    for day in range(last_day+1, last_day+ 28 +1):
+        dt[f"d_{day}"] = np.nan
+
+    dt = pd.melt(dt,
+                    id_vars = catcols,
+                    value_vars = [col for col in dt.columns if col.startswith("d_")],
+                    var_name = "d",
+                    value_name = "sales")
+
+    dt = dt.merge(cal, on= "d", copy = False)
+    dt = dt.merge(prices, on = ["store_id", "item_id", "wm_yr_wk"], copy = False)
+    dt['d'] = [x[2:] for x in dt['d']]
+    dt['d'] = dt['d'].astype("int16")
+    print(dt.head())
+    print('creating test dataframe...')
+    train_mask = dt['d'] <= last_day
+    test_mask = dt['d'] > (last_day - 100)
+
+    train_dt = dt[train_mask].reset_index(drop=True)
+    test_dt = dt[test_mask].reset_index(drop=True)
+    train_dt.to_feather(settings.TRAIN_DATA)
+    test_dt.to_feather(settings.TEST_DATA)
+
+def save_score(model_name, feature_name, params, CV_score):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    d = {'date and time': dt_string, 'model_name': model_name, 'feature_name': feature_name, 'train_file': '{0}.trn.feather'.format(feature_name), 'metric used': params['metric'],
+            'CV score' : CV_score, 'submission': '{0}.{1}.sub.csv'.format(model_name, feature_name)} #make better validation scheme
+    
+    df = pd.DataFrame(data=d, index=[0])
+    df.to_csv(os.path.join(settings.METRIC_DIR, '{0}.{1}.score.csv'.format(model_name, feature_name)))
+
+if __name__ == "__main__":
+    #make_out_directories() # run this code to create output directories 
+    make_test_train()
