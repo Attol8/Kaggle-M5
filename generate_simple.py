1c005e487f52fae6f56fa74bb0bac5a5aa60f211
Attol8 <jacopoattolini@gmail.com>
2020-05-01 21:59:04 +0200

    fixed d dtype

--- generate_simple.py
+++ generate_simple.py
@@ -62,7 +62,7 @@ def generate_feature(feature_name):
     print('Load Main Data')
     CAL_DTYPES={"event_name_1": "category", "event_name_2": "category", "event_type_1": "category", 
     "event_type_2": "category", "weekday": "category", 'wm_yr_wk': 'int16', "wday": "int16",
-    "month": "int16", "year": "int16", "snap_CA": "float32", 'snap_TX': 'float32', 'snap_WI': 'float32' }
+    "month": "int16", "year": "int16", "snap_CA": "float32", 'snap_TX': 'float32', 'snap_WI': 'float32', 'd' : 'int16' }
     PRICE_DTYPES = {"store_id": "category", "item_id": "category", "wm_yr_wk": "int16","sell_price":"float32" }
     train = pd.read_feather(settings.TRAIN_DATA)
     test = pd.read_feather(settings.TEST_DATA)
