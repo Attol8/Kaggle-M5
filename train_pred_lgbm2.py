ccef8615a5e12cefdec1125dc18631654c429271
Attol8 <jacopoattolini@gmail.com>
2020-05-06 15:48:35 +0200

    code refactor

--- train_pred_lgbm2.py
+++ train_pred_lgbm2.py
@@ -16,6 +16,35 @@ def seed_everything(seed=0):
     random.seed(seed)
     np.random.seed(seed) 
 
+def make_test_feature(df):
+    #create lags features
+    lags = [7, 28]
+    lag_cols = [f"lag_{lag}" for lag in lags ]
+    for lag, lag_col in zip(lags, lag_cols):
+        df[lag_col] = df[["id","sales"]].groupby("id")["sales"].shift(lag)
+
+    #create windows features
+    wins = [7, 28]
+    for win in wins :
+        for lag,lag_col in zip(lags, lag_cols):
+            df[f"rmean_{lag}_{win}"] = df[["id", lag_col]].groupby("id")[lag_col].transform(lambda x : x.rolling(win).mean())
+
+    date_features = {
+        "wday": "weekday",
+        "week": "weekofyear",
+        "month": "month",
+        "quarter": "quarter",
+        "year": "year",
+        "mday": "day",
+    }
+
+    for date_feat_name, date_feat_func in date_features.items():
+        if date_feat_name in df.columns:
+            df[date_feat_name] = df[date_feat_name].astype("int16")
+        else:
+            df[date_feat_name] = getattr(df["date"].dt, date_feat_func).astype("int16")
+    return df
+
 def train(feature_name, model_name, lgb_params):
 
     #load initial train set
@@ -56,11 +85,23 @@ def train(feature_name, model_name, lgb_params):
     model_path = os.path.join(settings.MODEL_DIR, '{0}.{1}.bin'.format(model_name, feature_name))
     pickle.dump(estimator, open(model_path, 'wb'))
 
-    #save validation metrics
-    print('Saving Validation Score...')
-    y_pred = estimator.predict(X_valid[features_columns])
+    #save metrics
+    save_metrics(X_valid, y_valid, model_name, feature_name)
+
+
+def save_metrics(X_valid, y_valid):
+    #save validation metric
+    print('Predicting and saving validation metric')
+    model_path = os.path.join(settings.MODEL_DIR, '{0}.{1}.bin'.format(model_name, feature_name))
+    estimator = pickle.load(open(model_path, 'rb'))
+    y_pred = estimator.predict(X_valid)
     rmse = sqrt(mean_squared_error(y_valid, y_pred)) #change metrics according to the one you use
-    save_score(model_name, feature_name, params= lgb_params, CV_score=rmse)
+    save_score(model_name, feature_name, params= lgb_params, CV_score=rmse)  
+
+    with open(os.path.join(settings.METRIC_DIR, '{0}.{1}.fmap'.format(model_name, feature_name)), 'w') as f:
+        for i, col in enumerate(X_valid.columns):
+            f.write('{}\t{}\tq\n'.format(i, col))
+
 
 def predict(feature_name, model_name):
 
@@ -71,6 +112,7 @@ def predict(feature_name, model_name):
     test_Poe = pd.read_feather(os.path.join(settings.FEATURE_DIR, '{0}.tst.feather'.format('Poe')))
     test_simple = pd.read_feather(os.path.join(settings.FEATURE_DIR, '{0}.tst.feather'.format('simple')))
     X_tst = pd.concat([test_Poe, test_simple], axis=1)
+    X_tst = X_tst.loc[:,~X_tst.columns.duplicated()]
     del test_Poe
     del test_simple
 
@@ -85,11 +127,13 @@ def predict(feature_name, model_name):
         estimator = pickle.load(open(model_path, 'rb'))
 
         grid_df = X_tst.copy()
+        grid_df = make_test_feature(grid_df)
 
         useless_cols = ["id", "date", "sales","d", "wm_yr_wk", "weekday"]
         features_columns = grid_df.columns[~grid_df.columns.isin(useless_cols)]
         day_mask = grid_df['date'] == str((last_day+datetime.timedelta(PREDICT_DAY)))
-        X_tst['sales'][day_mask] = estimator.predict(grid_df[day_mask][features_columns])
+        y_predict = estimator.predict(grid_df[day_mask][features_columns])
+        X_tst['sales'][day_mask] = y_predict
         
         # Make good column naming and add 
         # to all_preds DataFrame
@@ -110,7 +154,7 @@ def predict(feature_name, model_name):
     all_preds = all_preds.reset_index(drop=True)
     submission = pd.read_csv(settings.SAMPLE_SUBMISSION)[['id']]
     submission = submission.merge(all_preds, on=['id'], how='left').fillna(0)
-    submission.to_csv(os.path.join(settings.SUBMISSION_DIR, '{0}.{1}.sub.csv'.format(model_name, feature_name)))        
+    submission.to_csv(os.path.join(settings.SUBMISSION_DIR, '{0}.{1}.sub.csv'.format(model_name, feature_name)))
 
 if __name__ == "__main__":
     
@@ -133,8 +177,8 @@ if __name__ == "__main__":
                     'boost_from_average': False,
                     'verbose': -1,
                 }
-    train(feature_name, model_name, lgb_params)
-    predict(feature_name, model_name)
+    #train(feature_name, model_name, lgb_params)
+    #predict(feature_name, model_name)
     
 
 
