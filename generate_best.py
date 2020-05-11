f1f948e3dcb11e248bc314f197544156f99983fe
Attol8 <jacopoattolini@gmail.com>
2020-05-11 19:16:40 +0200

    code refactor and model 4

---
+++ generate_best.py
@@ -0,0 +1,104 @@
+from  datetime import datetime, timedelta
+import gc
+import numpy as np, pandas as pd
+import settings 
+import logging
+import os, sys, gc, time, warnings, pickle, psutil, random
+from math import ceil
+
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                       df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+def generate_feature(feature_name, is_train=True):
+    '''creates new features for train and test set then saves them separetely in the correct folder'''
+    # Here are reafing all our data 
+    # without any limitations and dtype modification
+
+    print('Load Main Data')
+    if is_train:
+        dt = pd.read_feather(settings.TRAIN_DATA)
+    else:
+        dt = pd.read_feather(settings.TEST_DATA)
+
+    lags = [7, 28]
+    lag_cols = [f"lag_{lag}" for lag in lags ]
+    for lag, lag_col in zip(lags, lag_cols):
+        dt[lag_col] = dt[["id","sales"]].groupby("id")["sales"].shift(lag)
+
+    wins = [7, 28]
+    for win in wins :
+        for lag,lag_col in zip(lags, lag_cols):
+            dt[f"rmean_{lag}_{win}"] = dt[["id", lag_col]].groupby("id")[lag_col].transform(lambda x : x.rolling(win).mean())
+    
+    date_features = {
+        
+        "wday": "weekday",
+        "week": "weekofyear",
+        "month": "month",
+        "quarter": "quarter",
+        "year": "year",
+        "mday": "day",
+#         "ime": "is_month_end",
+#         "ims": "is_month_start",
+    }
+    
+#     dt.drop(["d", "wm_yr_wk", "weekday"], axis=1, inplace = True)
+    
+    for date_feat_name, date_feat_func in date_features.items():
+        if date_feat_name in dt.columns:
+            dt[date_feat_name] = dt[date_feat_name].astype("int16")
+        else:
+            dt[date_feat_name] = getattr(dt["date"].dt, date_feat_func).astype("int16")
+    
+    #drop useless columns and NA
+    useless_cols = ["wm_yr_wk", "weekday"]
+    train_cols = dt.columns[~dt.columns.isin(useless_cols)]
+    dt = dt[train_cols]
+    if is_train: dt.dropna(inplace = True)
+    
+    ########################### Final list of features
+    #################################################################################
+    print(dt.info())
+
+    with open(os.path.join(settings.FEATURE_DIR, '{0}.fmap'.format(feature_name)), 'w') as f:
+        for i, col in enumerate(dt.columns):
+            f.write('{}\t{}\tq\n'.format(i, col))
+
+    #trn_tst = trn_tst.astype('float32', errors='ignore')
+    
+    #trn_tst = trn_tst.reset_index(drop=True)
+    #save to feathers
+    if is_train:
+        dt.reset_index().to_feather(os.path.join(settings.FEATURE_DIR, '{0}.trn.feather'.format(feature_name)))
+    else:
+        dt.reset_index().to_feather(os.path.join(settings.FEATURE_DIR, '{0}.tst.feather'.format(feature_name)))
+
+if __name__ == "__main__":
+    generate_feature(feature_name = "best", is_train=False)
+    generate_feature(feature_name = "best", is_train=True)
+
---
+++ train_pred_lgbm4.py
@@ -0,0 +1,203 @@
+import lightgbm as lgb
+import pandas as pd
+import datetime
+import pickle
+import random
+import numpy as np
+import time
+import os
+import gc
+import settings
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+from utils import save_score, reduce_mem_usage
+
+
+def create_lag_features_for_test(df, day):
+    print(f'creating lag and windows feature for day {day}')
+    #create lags features
+    df['d'] = df['d'].str[-4:]
+    df['d'] = df['d'].astype('float32')
+    lags = [7, 28]
+    lag_cols = [f"lag_{lag}" for lag in lags]
+    for lag, lag_col in zip(lags, lag_cols):
+        df.loc[df.d == day, lag_col] = \
+        df.loc[df.d == day-lag, 'sales'].values
+
+    wins = [7, 28]
+    for window in wins :
+        for shift in lags:
+            df_window = df[(df.d <= day-shift) & (df.d > day-(shift+window))] #filter for date <= day-shift and date > 
+            df_window_grouped = df_window.groupby("id").agg({'sales':'mean'}).reindex(df.loc[df.d == day,'id'])
+            df.loc[df.d == day,f"rmean_{shift}_{window}"] = \
+                df_window_grouped.sales.values
+    return df
+
+def save_val_set(feature_name, model_name):
+    train_df = pd.read_feather(os.path.join(settings.FEATURE_DIR, '{0}.trn.feather'.format('best')))
+
+    print('saving validation set')
+    last_day = datetime.date(2016, 4, 24)
+    #last_day_n = 1913
+    P_HORIZON = datetime.timedelta(28)  
+    valid_mask = train_df['date']>str((last_day-P_HORIZON)) #mask for validation set, it is our validation  strategy rn 
+
+    X_val = train_df[valid_mask]
+    X_val.to_csv(os.path.join(settings.VAL_DIR, 'val.{0}.{1}.csv'.format(model_name, feature_name)), index=False)
+
+def train(feature_name, model_name, lgb_params):
+
+    for store_id in list(range(10)):   #stores are encoded
+        print('loading store {0} dataset'.format(store_id))
+        train_df = pd.read_feather(os.path.join(settings.FEATURE_DIR, '{0}.trn.feather'.format('best')))
+        train_df = train_df[train_df['store_id']==store_id]
+        train_df, _ = reduce_mem_usage(train_df)       
+
+        #prepare data for lgb
+        cat_feats = ['item_id', 'dept_id', 'cat_id'] + ["event_name_1", "event_name_2", "event_type_1", "event_type_2"]
+        useless_cols = ['id', 'date', "d", "sales"]
+        features_columns = train_df.columns[~train_df.columns.isin(useless_cols)]
+
+        last_day = datetime.date(2016, 4, 24)
+        P_HORIZON = datetime.timedelta(28)
+        valid_mask = train_df['date']>str((last_day-P_HORIZON)) #mask for validation set, it is our validation  strategy rn
+
+        X_train, y_train = train_df[features_columns], train_df['sales']
+        X_valid, y_valid = train_df[valid_mask][features_columns], train_df[valid_mask]['sales']
+        del train_df; gc.collect()
+
+        train_data = lgb.Dataset(X_train, label= y_train, categorical_feature=cat_feats, free_raw_data=False)
+        valid_data = lgb.Dataset(X_valid, label= y_valid, categorical_feature=cat_feats, free_raw_data=False)
+
+        estimator = lgb.train(lgb_params,
+                            train_data,
+                            valid_sets = [valid_data],
+                            verbose_eval = 100,
+                            categorical_feature=cat_feats
+                            )
+
+        print('Saving model...')
+        # save model to file
+        model_path = os.path.join(settings.MODEL_DIR, '{0}.{1}.{2}.bin'.format(model_name, feature_name, store_id))
+        pickle.dump(estimator, open(model_path, 'wb'))
+
+def save_metrics(feature_name, model_name):
+    #save validation metric
+    print('saving validation metric')
+    valid_data = pd.read_csv(os.path.join(settings.VAL_DIR, 'val.{0}.{1}.csv'.format(model_name, feature_name)))
+    valid_data.reset_index(drop=True, inplace=True)
+    y_valid = valid_data['sales']
+
+    grid_df = valid_data.copy()
+    grid_df['sales'] = 0
+    
+    for store_id in list(range(10)):
+        store_mask = grid_df['store_id']==store_id
+        useless_cols = ['date',"sales","d"]
+        features_columns = grid_df.columns[~grid_df.columns.isin(useless_cols)]
+
+        model_path = os.path.join(settings.MODEL_DIR, '{0}.{1}.{2}.bin'.format(model_name, feature_name, store_id))
+        estimator = pickle.load(open(model_path, 'rb'))
+
+        grid_df.loc[store_mask, 'sales'] = estimator.predict(grid_df[store_mask][features_columns])
+        y_pred_store = grid_df.loc[store_mask, 'sales']
+        y_valid_store = valid_data.loc[valid_data['store_id']==store_id, 'sales']
+        #y_valid = y_valid.values #convert to np array as y_pred is np array
+        rmse = sqrt(mean_squared_error(y_valid_store, y_pred_store))
+        print('store {1} validation rmse is : {0}'.format(rmse, store_id))
+
+    y_valid, y_pred = valid_data['sales'], grid_df['sales']
+    rmse = sqrt(mean_squared_error(y_valid, y_pred)) #change metrics according to the one you use
+    print('validation rmse is : {0}'.format(rmse))
+    save_score(model_name, feature_name, params= lgb_params, CV_score=rmse)  
+
+    with open(os.path.join(settings.METRIC_DIR, '{0}.{1}.fmap'.format(model_name, feature_name)), 'w') as f:
+        for i, col in enumerate(features_columns):
+            f.write('{}\t{}\tq\n'.format(i, col))
+
+def predict(feature_name, model_name):
+
+    print('initiating prediction dataframe...')
+    all_preds = pd.DataFrame() # Create Dummy DataFrame to store predictions
+
+    #load initial test set
+    X_tst = pd.read_feather(os.path.join(settings.FEATURE_DIR, '{0}.tst.feather'.format('best')))
+    last_day_n = 1913
+    X_tst.reset_index(drop=True, inplace=True)
+    print(f'X_tst shape: {X_tst.shape}')
+    main_time = time.time()
+
+    for PREDICT_DAY in range(1,29):    
+        print('Predict | Day:', PREDICT_DAY)
+        start_time = time.time()
+        grid_df = X_tst.copy()
+        day = last_day_n + PREDICT_DAY
+        grid_df = create_lag_features_for_test(grid_df, day)
+        print(f'missing columns {[x for x in grid_df.columns if x not in X_tst.columns]}')
+        print(f'missing values total {grid_df.isnull().sum().sum()}')
+        print(f'columns with missing values: {grid_df.columns[grid_df.isnull().any()].tolist()}')
+
+        for store_id in list(range(10)):
+            model_path = os.path.join(settings.MODEL_DIR, '{0}.{1}.{2}.bin'.format(model_name, feature_name, store_id))
+            estimator = pickle.load(open(model_path, 'rb'))
+            useless_cols = ['id', 'date',"sales","d"]
+            features_columns = grid_df.columns[~grid_df.columns.isin(useless_cols)]
+            day_mask = grid_df['d'] == day
+            store_mask = grid_df['store_id']==store_id
+            mask = day_mask & store_mask
+            #print(X_tst_store.columns)
+            X_tst.loc[mask, 'sales'] = estimator.predict(grid_df[mask][features_columns])
+        
+        # Make good column naming and add 
+        # to all_preds DataFrame
+        temp_df = X_tst[day_mask][['id','sales']]
+        temp_df.columns = ['id','F'+str(PREDICT_DAY)]
+
+        if 'id' in list(all_preds):
+            all_preds = all_preds.merge(temp_df, on=['id'], how='left')
+        else:
+            all_preds = temp_df.copy()
+            
+        print('#'*10, ' %0.2f min round |' % ((time.time() - start_time) / 60),
+                    ' %0.2f min total |' % ((time.time() - main_time) / 60),
+                    ' %0.2f day sales |' % (temp_df['F'+str(PREDICT_DAY)].sum()),
+                    '\n')
+        del temp_df
+
+    #make submission
+    all_preds = all_preds.reset_index(drop=True)
+    submission = pd.read_csv(settings.SAMPLE_SUBMISSION)[['id']]
+    submission = submission.merge(all_preds, on=['id'], how='left').fillna(0)
+    submission.to_csv(os.path.join(settings.SUBMISSION_DIR, '{0}.{1}.sub.csv'.format(model_name, feature_name)), index=False)
+
+if __name__ == "__main__":
+    
+    feature_name = "best" #inspired from https://www.kaggle.com/poedator/m5-under-0-50-optimized
+    model_name = 'lgbm3'
+    lgb_params = {
+                    'boosting_type': 'gbdt',
+                    'objective': 'tweedie',
+                    'tweedie_variance_power': 1.1,
+                    'is_training_metric': True,
+                    'metric': 'rmse',
+                    'subsample': 0.5,
+                    'subsample_freq': 1,
+                    'learning_rate': 0.03,
+                    'num_leaves': 2**11-1,
+                    'min_data_in_leaf': 2**12-1,
+                    'feature_fraction': 0.5,
+                    'max_bin': 100,
+                    'n_estimators': 1300,
+                    'boost_from_average': False,
+                    'verbose': -1,
+                }
+
+    #save_val_set(feature_name, model_name)
+    #train(feature_name, model_name, lgb_params)
+    #save_metrics(feature_name, model_name)
+    predict(feature_name, model_name)
+    
+
+
+
+
