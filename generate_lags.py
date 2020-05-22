e90a18d3a94b2211db5b0f3b22b6857a4b735bdb
Attol8 <jacopoattolini@gmail.com>
2020-05-22 19:52:21 +0200

    fixed lags

--- generate_lags.py
+++ generate_lags.py
@@ -36,11 +36,11 @@ def generate_feature(feature_name, is_train=True):
         print('Shifting period:', d_shift)
         for d_window in [7, 14, 30, 60, 120]:
             col_name_m = 'rmean_'+str(d_shift)+'_'+str(d_window)
-            dt[col_name_m] = dt[["id","sales"]].groupby(['id'])["sales"].transform(lambda x: x.shift(d_shift).rolling(d_window).mean()).astype(np.float16)
+            dt[col_name_m] = dt[["id", f"lag_{d_shift}"]].groupby(['id'])[f"lag_{d_shift}"].transform(lambda x: x.rolling(d_window).mean()).astype(np.float16)
             col_name_s = 'smean_'+str(d_shift)+'_'+str(d_window)
-            dt[col_name_s] = dt[["id","sales"]].groupby(['id'])["sales"].transform(lambda x: x.shift(d_shift).rolling(d_window).std()).astype(np.float16)
+            dt[col_name_s] = dt[["id",f"lag_{d_shift}"]].groupby(['id'])[f"lag_{d_shift}"].transform(lambda x: x.rolling(d_window).std()).astype(np.float16)
             col_name_max = 'max_sales_'+str(d_shift)+'_'+str(d_window)
-            dt[col_name_max] = dt[["id","sales"]].groupby(['id'])["sales"].transform(lambda x: x.shift(d_shift).rolling(d_window).max()).astype(np.float16)
+            dt[col_name_max] = dt[["id",f"lag_{d_shift}"]].groupby(['id'])[f"lag_{d_shift}"].transform(lambda x: x.rolling(d_window).max()).astype(np.float16)
 
 
     date_features = {
