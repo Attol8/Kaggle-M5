c48e87181ed55ef43096b2e325a034737f146891
Attol8 <jacopoattolini@gmail.com>
2020-05-25 14:06:07 +0200

    try 2

--- train_pred_lgbm5gap.py
+++ train_pred_lgbm5gap.py
@@ -310,10 +310,10 @@ if __name__ == "__main__":
 
     features_l = ['best', 'simple', 'lags3']
     #numbers_check(features_l)
-    save_val_set(feature_name, model_name, features_l=features_l)
+    #save_val_set(feature_name, model_name, features_l=features_l)
     train(feature_name, model_name, lgb_params, features_l=features_l, features_selection=True)
-    save_metrics(feature_name, model_name) #uncomment when running features tests
-    #predict(feature_name, model_name, features_l=features_l)
+    #save_metrics(feature_name, model_name) #uncomment when running features tests
+    predict(feature_name, model_name, features_l=features_l)
     
 
 
